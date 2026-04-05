b859a5a5171e24ad32194ced795278aa09285a89
suswin-z <suswin@zigmaglobal.in>
2026-04-05 17:25:52 +0530

    sunday change 3.1

--- campaign_os/settings.py
+++ campaign_os/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'campaign_os',
         'USER': 'root',
-        'PASSWORD': 'Su$i0410',
-        # 'PASSWORD': 'admin@123',
+        # 'PASSWORD': 'Su$i0410',
+        'PASSWORD': 'admin@123',
 
         'HOST': 'localhost',
         'PORT': '3306',
